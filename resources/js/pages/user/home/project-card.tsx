0d38c50fa4fdfea080dd3cc14356b3f7b045fdb7
Neoscripter <sange0337@gmail.com>
2025-11-16 12:52:00 +0800

    make project card skeleton

--- resources/js/pages/user/home/project-card.tsx
+++ resources/js/pages/user/home/project-card.tsx
@@ -11,7 +11,7 @@ const ProjectCard: FC<{ project: ProjectType }> = ({ project }) => {
     return (
         <li
             class={cn(
-                'shadow-project max-w-90 justify-self-center overflow-clip rounded-md transition-transform duration-300 ease-in-out hover:scale-103 xl:max-w-100',
+                'shadow-project max-w-90 justify-self-center overflow-clip rounded-md transition-transform duration-300 ease-in-out select-none hover:scale-103 xl:max-w-100',
                 appearance.value === 'dark' && 'bg-muted',
             )}
         >
@@ -47,20 +47,18 @@ export const ProjectCardSkeleton = () => {
     return (
         <li
             class={cn(
-                'bg-muted project-slide border-accent-foreground/15 ease flex flex-col items-start gap-8 rounded-xl border py-7.5 pr-7 pl-6 select-none sm:flex-row sm:gap-10.5 sm:py-12 sm:pr-18 sm:pl-8 sm:text-base md:items-center lg:gap-12 lg:pt-12 lg:pr-17 lg:pb-18 lg:pl-10.5 lg:text-xl xl:pb-13',
+                'shadow-project max-w-90 justify-self-center overflow-clip rounded-md select-none xl:max-w-100',
             )}
         >
-            <div class="skeleton size-32 shrink-0 animate-pulse rounded-full md:size-40 lg:size-51" />
-            <div>
-                <p class="skeleton mb-6">
-                    Lorem ipsum dolor sit amet consectetur. In enim cursus odio
-                    accumsan. Id leo urna velit neque mattis id tellus arcu
-                    condimentum. Augue dictum dolor elementum convallis
-                    dignissim malesuada commodo ultrices.
-                </p>
+            <div class="skeleton aspect-5/6 w-full rounded-md" />
 
-                <p class="skeleton w-40 animate-pulse font-bold md:text-xl xl:text-2xl">
-                    Lorem
+            <div class="px-5 pt-5 pb-7 text-transparent sm:px-6 sm:pt-6">
+                <h4 class="skeleton mb-5.5 text-2xl font-medium xl:text-2xl">
+                    Lorem ipsum
+                </h4>
+                <p class="skeleton text-sm">
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                    Lorem ipsum Lorem ipsum
                 </p>
             </div>
         </li>
--- resources/js/pages/user/home/projects.tsx
+++ resources/js/pages/user/home/projects.tsx
@@ -1,6 +1,7 @@
 import ApiError from '@/components/user/ui/api-error';
+import PrimaryLink from '@/components/user/ui/primary-link';
 import { useFetch } from '@/hooks/use-fetch';
-import { ProjectResource, ProjectType } from '@/lib/types/projects';
+import { ProjectType } from '@/lib/types/projects';
 import { locale } from '@/signals/locale';
 import { cn } from '@/utils/cn';
 import { range } from '@/utils/range';
@@ -29,22 +30,31 @@ const Projects: FC<{ className?: string }> = ({ className }) => {
             <div className="relative mt-16 sm:mt-26 lg:mt-28">
                 <ul
                     className={cn(
-                        'grid gap-15 lg:gap-15 sm:gap-6.5 place-content-center sm:grid-cols-2 xl:grid-cols-3',
+                        'grid place-content-center gap-15 sm:grid-cols-2 sm:gap-6.5 lg:gap-15 xl:grid-cols-3',
                     )}
                     role="tablist"
                     aria-label={listLabel}
                     aria-live="polite"
                 >
                     {!loading
-                        ? projects?.slice(0,6).map((project, idx) => (
-                              <ProjectCard key={project.id} project={project} />
-                          ))
-                        : range(0, 8).map((skeleton) => (
+                        ? projects
+                              ?.slice(0, 6)
+                              .map((project, idx) => (
+                                  <ProjectCard
+                                      key={project.id}
+                                      project={project}
+                                  />
+                              ))
+                        : range(0, 5).map((skeleton) => (
                               <ProjectCardSkeleton
                                   key={`project-skeleton-${skeleton}`}
                               />
                           ))}
                 </ul>
+
+                <PrimaryLink href="/projects" className="mx-auto mt-22 w-fit">
+                    На страницу проектов
+                </PrimaryLink>
             </div>
         </div>
     );
