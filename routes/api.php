f05810307a9216ac464656bbed829d87a82aeb8d
Neoscripter <sange0337@gmail.com>
2026-01-14 14:03:47 +0800

    Remove throttle middleware

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\User\StackController;
 use App\Http\Controllers\User\VideoController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('throttle:60,1')->group(function () {
+// Route::middleware('throttle:60,1')->group(function () {
     Route::apiResource('/reviews', ReviewController::class)->only(['index', 'show']);
     Route::apiResource('/videos', VideoController::class)->only(['index', 'show']);
     Route::apiResource('/faqs', FaqController::class)->only(['index', 'show']);
@@ -17,4 +17,4 @@ Route::middleware('throttle:60,1')->group(function () {
     Route::apiResource('/stacks', StackController::class)->only(['index', 'show']);
     Route::apiResource('/categories', CategoryController::class)->only(['index']);
     Route::apiResource('/technologies', TechnologyController::class)->only(['index']);
-});
+// });
